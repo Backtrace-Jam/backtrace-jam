f38eae8d5b8c9d641e40c790a5d61fa252119396
Parven <parvendranofficial05@gmail.com>
2022-06-25 15:36:25 +0800

    added healthbar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HealthBar : MonoBehaviour
+{
+    public float health;
+    public Slider slider;
+
+    void Start()
+    {
+       // healthbar = GetComponent<Image>();
+
+    }
+
+
+    void Update()
+    {
+        slider.value = health;
+    }
+
+     void OnCollisionEnter(Collision obj)
+    {
+        if(obj.gameObject.tag == "Enemy")
+            health = health-10f;
+        HealthLoss();
+
+    }
+    
+    void HealthLoss()
+    {
+        if (health == 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
